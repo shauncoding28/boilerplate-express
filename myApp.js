f3d48a2047d7ea364995e1ba875ccc42394b0498
shauncoding28 <196330731+shauncoding28@users.noreply.github.com.>
2025-03-05 16:43:36 +0100

    update get request

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let app = express();
+const path = require('path');
+
 
 console.log('Hello World');
 
 
 app.get('/', (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+
+
 
 
 
